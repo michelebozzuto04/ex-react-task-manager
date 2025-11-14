36474691e8abc38a4bb37d4fdf71fcc5cd92df58
michelebozzuto04 <michele.bozzuto04@gmail.com>
2025-11-14 14:15:32 +0100

    Fixed Milestone 7

--- src/pages/TaskDetail.jsx
+++ src/pages/TaskDetail.jsx
@@ -1,14 +1,28 @@
 import {
     useParams,
 } from "react-router-dom";
+import { useGlobalContext } from '../contexts/GlobalContext'
+import { useState } from "react";
 
 function TaskDetail() {
 
     const { id } = useParams();
+    const { tasks } = useGlobalContext();
+    const currentTask = tasks[id - 1];
+
+    function handleTaskDelete() {
+        console.log(`Elimino task con id ${id}`)
+    }
 
     return (
         <div className="container">
             <h1>Task with id {id}</h1>
+            <h2>{currentTask.title}</h2>
+            <p>{currentTask.description}</p>
+            <p>{currentTask.status}</p>
+            <p>{currentTask.createdAt.toLocaleString()}</p>
+
+            <button onClick={handleTaskDelete}>Elimina task</button>
         </div>
     )
 }
